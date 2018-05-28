95274a70f8dce7d51f841699f0511da79e6decc9
nikowis <nikowis@gmail.com>
2018-05-29 00:38:22 +0200

    Final presentation.

--- scripts/program.py
+++ scripts/program.py
@@ -1,12 +1,24 @@
 import cv2 as cv
 import numpy as np
+import time
 
-img = imgbin = cv.imread('./../RecordedImage_GO-5000M-PGE_00-0C-DF-09-1B-B4_001.tif', cv.IMREAD_GRAYSCALE)
+idx = '2'
+start_time = time.time()
+
+img = imgbin = cv.imread('./../' + idx + '.tif', cv.IMREAD_GRAYSCALE)
 imgblur = cv.GaussianBlur(imgbin, (3, 3), 1)
+
+cv.imwrite('./../imgblur.png', imgblur)
+
 
 ret1, dark_reduct = cv.threshold(imgblur, 90, 255, cv.THRESH_BINARY)
 
 imgblur[dark_reduct < 1] = 0
+
+cv.imwrite('./../dark_reduct.png', dark_reduct)
+
+cv.imwrite('./../imgblurreducted.png', imgblur)
+
 
 imgblurblack = cv.adaptiveThreshold(imgblur, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 121, 2)
 cv.imwrite('./../blurblack.png', imgblurblack)
@@ -22,7 +34,7 @@ if circles is not None:
     circles = np.uint16(np.around(circles))
 
     for i in circles[0, :]:
-        if imgblurblack[i[1], i[0]] != 0 and imgblur[i[1], i[0]]>100 :
+        if imgblurblack[i[1], i[0]] != 0 and imgblur[i[1], i[0]] > 100:
             # draw the outer circle
             cv.circle(img, (i[0], i[1]), i[2], (0, 0, 0), 2)
             # draw the center of the circle
@@ -31,7 +43,8 @@ if circles is not None:
 
 print('Really found ', real_circles, ' circles')
 
+print("Time {0} seconds".format((time.time() - start_time)))
 
-cv.imwrite('./../circles.png', img)
+cv.imwrite('./../circles' + idx + '.png', img)
 
 print('Exiting')
